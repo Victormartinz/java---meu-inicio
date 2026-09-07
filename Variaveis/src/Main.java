ab9fd5be361c7bed0d7f077d7676a77bc328476a
VictorMss <martinssantosvictor@gmail.com>
2026-09-07 19:32:10 -0300

    Usando variaveis pela primeira vez em Java

---
+++ Variaveis/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 21;
+        String nome = "Victor";
+        String Profissao = "Estagiario de T.I";
+        String meuCurso = "Sistemas de Informação";
+
+        System.out.println("Meu nome é " + nome + ", Tenho " + idade + " Anos. " +
+                "Eu sou " + Profissao + " E faço faculdade de " + meuCurso);
+
+    }
+
+}
