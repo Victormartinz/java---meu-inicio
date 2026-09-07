297ceb520045f15e0d3e5160350dc05f97f87109
VictorMss <martinssantosvictor@gmail.com>
2026-09-07 20:16:37 -0300

    Usando tipos primitivos em Java

--- Variaveis/src/Main.java
+++ Variaveis/src/Main.java
@@ -5,9 +5,18 @@ public class Main {
         String nome = "Victor";
         String Profissao = "Estagiario de T.I";
         String meuCurso = "Sistemas de Informação";
+        double salarioMensal = 1200.00;
+        char genero = 'M';
+        boolean estaEmpregado = true;
 
-        System.out.println("Meu nome é " + nome + ", Tenho " + idade + " Anos. " +
-                "Eu sou " + Profissao + " E faço faculdade de " + meuCurso);
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Profissão: " + Profissao);
+        System.out.println("Salário Mensal: "  + salarioMensal);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Graduação: " + meuCurso);
+        System.out.println("Está empregado: " + estaEmpregado);
+
 
     }
 
