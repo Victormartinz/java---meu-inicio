dbe875934db1d371de3a5f9e6d109bf5e109e3ac
VictorMss <martinssantosvictor@gmail.com>
2026-09-20 21:32:01 -0300

    Usando expressões comparativas em Java

---
+++ Comparativos/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int pao = 5;
+        int cafe = 10;
+
+        System.out.println(pao == cafe);
+        System.out.println(pao != cafe);
+        System.out.println(pao > cafe);
+        System.out.println(pao < cafe);
+        System.out.println(pao >= cafe);
+        System.out.println(pao <= cafe);
+    }
+}
